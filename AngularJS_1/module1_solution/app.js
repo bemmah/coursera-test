5b49faf75e1938778759bd84f074ef63fc6f77e7
Mavis <bemmah@gmail.com>
2019-01-06 20:54:34 -0600

    module 1 solution complete

--- AngularJS_1/module1_solution/app.js
+++ AngularJS_1/module1_solution/app.js
@@ -1,16 +1,28 @@
-(function(){
-    'use strict';
-    
-    angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
-    
-    LunchCheckController.$inject = ['$scope'];
-    function LunchCheckController($scope){
-        
-        
-        
-        $scope.button = function(){
-            $scope.lunchItemsMessage = "test";
-        }
-    }
-    
+(function () {
+'use strict';
+
+angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope){
+   
+  $scope.checkItems = function(){
+     if(!$scope.lunchItems){
+        $scope.feedback = "Please enter data first";
+     }else if($scope.lunchItems){
+        var enteredList = $scope.lunchItems;
+        var list = enteredList.split(',');
+        if(list.length <= 3)
+         $scope.feedback = "Enjoy!";
+        else
+         $scope.feedback = "Too much!";
+     }
+
+  };
+  
+  $scope.returnString = function(){
+     
+  }
+}
+
 })();
